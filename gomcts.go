34805ce8d520efcc968c8ec49f1fe2b837dc16f6
John Lester <johnlester@gmail.com>
2012-10-20 22:40:17 -0400

    node tree links based on slices

--- gomcts.go
+++ gomcts.go
@@ -3,6 +3,7 @@ package gomcts
 import (
 	"bufio"
 	"log"
+	"math/rand"
 	"os"
 )
 
@@ -18,20 +19,34 @@ const ()
 // TreeNode structure, New function, and methods
 
 type TreeNode struct {
-	State          *gameState
-	generatingMove string
-	Parent         *TreeNode
-	Children       [string]TreeNode
-	BestChildMove  string
-	bestChildScore float64
-	VisitCount     uint64
-	shuffledMoves  []string
-	nextMoveToTry  uint16
+	State               *GameState
+	Parent              *TreeNode
+	GeneratingMove      string
+	SortedPossibleMoves []string
+	NumberOfChildren    int
+	Children            []*TreeNode
+	orderToTryMoves     []int
+	NextMoveToTry       int
+	VisitCount          []uint64
+	CumulativeScore     []float64
+	BestChildMove       int
+	BestChildScore      float64
 }
 
-func NewNode(state *gameState, parent *TreeNode, generatingMove string) *TreeNode {
-	newNode := TreeNode{State: state, Parent: parent, generatingMove: generatingMove} //rest of fields should start with "zeroed" values
-	newNode.shuffledMoves = shuffle(newNode.PossibleMoves)
+func NewNode(state *GameState, parent *TreeNode, generatingMove string) *TreeNode {
+	sortedMoves := state.PossibleMoves
+	numChilds := len(sortedMoves)
+	return TreeNode{State: state,
+		Parent:              parent,
+		GeneratingMove:      generatingMove,
+		SortedPossibleMoves: sortedMoves,
+		NumberOfChildren:    numChilds,
+		Children:            make([]*TreeNode, numChilds),
+		orderToTryMoves:     Perm(numChilds),
+		VisitCount:          make([]unit64, numChilds),
+		CumulativeScore:     make([]float64, numChilds),
+		BestChildMove:       -1,
+		BestChildScore:      -1E10} //rest of fields should start with "zeroed" values
 }
 
 func (node *TreeNode) bestMoveFromNIterations(iterationBudget uint64) string {
@@ -39,12 +54,22 @@ func (node *TreeNode) bestMoveFromNIterations(iterationBudget uint64) string {
 }
 
 func (node *TreeNode) doTreePolicy() *TreeNode {
-
+	selectedNode := node
+	for selectedNode.State.IsNotTerminal() {
+		if selectedNode.IsFullyExpanded() {
+			selectedNode = selectedNode.BestChild(1.0)
+		}
+	}
 }
 
 //Two-player version using negamax 
 func (node *TreeNode) backpropagate(reward float64) {
 
+}
+
+func (node *TreeNode) BestChild(factor float64) *TreeNode {
+	var bestUCTValue float64 = -1E10
+	var selectedChild *TreeNode
 }
 
 /////////////////////////////////////////////////////////////
