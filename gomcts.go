eb5b5ac758bb6a7d0ed2b16dc6ae721d6f17a1f5
John Lester <johnlester@gmail.com>
2012-10-21 23:12:18 -0400

    compiles so far

--- gomcts.go
+++ gomcts.go
@@ -1,15 +1,19 @@
 package gomcts
 
 import (
-	"bufio"
-	"log"
+	// "bufio"
+	// "log"
+	"math"
 	"math/rand"
-	"os"
+	// "os"
 )
 
 var ()
 
-const ()
+const (
+	startingBestChildScore = -1E9
+	oneDividedBySqrtOfTwo  = 0.70710678118
+)
 
 // type Tree struct {
 // 	root *treeNode
@@ -19,7 +23,7 @@ const ()
 // TreeNode structure, New function, and methods
 
 type TreeNode struct {
-	State               *GameState
+	State               GameState
 	Parent              *TreeNode
 	GeneratingMove      string
 	SortedPossibleMoves []string
@@ -27,30 +31,30 @@ type TreeNode struct {
 	Children            []*TreeNode
 	orderToTryMoves     []int
 	NextMoveToTry       int
-	VisitCount          []uint64
-	CumulativeScore     []float64
-	BestChildMove       int
-	BestChildScore      float64
+	VisitCount          float64
+	CumulativeScore     float64 //Scores are to be between 0 (loss) and 1 (win) 
 }
 
-func NewNode(state *GameState, parent *TreeNode, generatingMove string) *TreeNode {
-	sortedMoves := state.PossibleMoves
+func NewNode(state GameState, parent *TreeNode, generatingMove string) *TreeNode {
+	sortedMoves := state.PossibleMoves()
 	numChilds := len(sortedMoves)
-	return TreeNode{State: state,
+	return &TreeNode{State: state,
 		Parent:              parent,
 		GeneratingMove:      generatingMove,
 		SortedPossibleMoves: sortedMoves,
 		NumberOfChildren:    numChilds,
 		Children:            make([]*TreeNode, numChilds),
-		orderToTryMoves:     Perm(numChilds),
-		VisitCount:          make([]unit64, numChilds),
-		CumulativeScore:     make([]float64, numChilds),
-		BestChildMove:       -1,
-		BestChildScore:      -1E10} //rest of fields should start with "zeroed" values
+		orderToTryMoves:     rand.Perm(numChilds)}
+	//rest of fields should start with "zeroed" values
 }
 
-func (node *TreeNode) bestMoveFromNIterations(iterationBudget uint64) string {
-
+func (node *TreeNode) bestMoveFromNIterations(iterationBudget int) string {
+	for i := 0; i < iterationBudget; i++ {
+		currentNode := node.doTreePolicy()
+		reward := currentNode.State.RewardFromRandomPlayout()
+		currentNode.backpropagateReward(reward)
+	}
+	return node.BestChild(0.0).GeneratingMove
 }
 
 func (node *TreeNode) doTreePolicy() *TreeNode {
@@ -58,22 +62,56 @@ func (node *TreeNode) doTreePolicy() *TreeNode {
 	for selectedNode.State.IsNotTerminal() {
 		if selectedNode.IsFullyExpanded() {
 			selectedNode = selectedNode.BestChild(1.0)
+		} else {
+			return selectedNode.NewChild()
 		}
 	}
+	return selectedNode //must be terminal, I think
 }
 
 //Two-player version using negamax 
-func (node *TreeNode) backpropagate(reward float64) {
-
+func (node *TreeNode) backpropagateReward(reward float64) {
+	currentNode := node
+	currentReward := reward
+	for currentNode.Parent != nil {
+		currentNode.VisitCount += 1.0
+		currentNode.CumulativeScore += reward
+		currentReward = currentReward * -1.0
+		currentNode = currentNode.Parent
+	}
 }
 
 func (node *TreeNode) BestChild(factor float64) *TreeNode {
-	var bestUCTValue float64 = -1E10
+	var bestUctValue float64 = -1E10
 	var selectedChild *TreeNode
+	for _, child := range node.Children {
+		uctValue := (child.CumulativeScore / child.VisitCount) + factor*math.Sqrt(2.0*math.Log(node.VisitCount)/child.VisitCount)
+		//Need random tie-breaker?
+		if uctValue >= bestUctValue {
+			selectedChild = child
+			bestUctValue = uctValue
+		}
+	}
+	return selectedChild
+}
+
+func (node *TreeNode) IsFullyExpanded() bool {
+	return (node.NextMoveToTry == node.NumberOfChildren)
+}
+
+func (node *TreeNode) NewChild() *TreeNode {
+	//NextMoveToTry should < NumberOfChildren
+	nextMove := node.SortedPossibleMoves[node.orderToTryMoves[node.NextMoveToTry]]
+	newState := node.State.NewGameStateFromMove(nextMove)
+	newChild := NewNode(newState, node, nextMove)
+	node.Children[node.NextMoveToTry] = newChild
+	node.NextMoveToTry++
+	return newChild
 }
 
 /////////////////////////////////////////////////////////////
-// GameState structure and interface
+// GameState interface
+/////////////////////////////////////////////////////////////
 
 type GameState interface {
 	PossibleMoves() []string
@@ -82,4 +120,33 @@ type GameState interface {
 	//copy() GameState
 	NewGameStateFromMove(move string) GameState
 	RewardFromRandomPlayout() float64
+}
+
+/////////////////////////////////////////////////////////////
+// GameState implementation of my made-up game 123ToTen
+/////////////////////////////////////////////////////////////
+
+type GameState123ToTen struct {
+	Total uint8
+	PlayerBMove bool
+}
+
+func (gstate GameState123ToTen) PossibleMoves() []string {
+	moves := []string{"1","2","3"}
+	return moves
+}
+
+func (gstate GameState123ToTen) IsNotTerminal() bool {
+	return gstate.Total < 10
+}
+
+func (gstate GameState123ToTen) TerminalReward() float64 {
+	// error if not terminal?
+	var reward float64
+	if gstate.PlayerBMove {
+		reward = 0.0
+	} else {
+		reward = 1.0
+	}
+	return reward
 }
